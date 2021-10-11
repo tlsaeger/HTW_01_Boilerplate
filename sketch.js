51d4b91ab3d44f11536149f6a1e81c68233a8497
Tom Saeger <tlsaeger@icloud.com>
2021-10-11 13:37:08 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+/* Zoom Background 3000 
+Workshop »AI X Design – Rage against the machine?« @ HTW Berlin 
+held by Tom-Lucas Säger 🌍 tlsaeger.de 🐦 @t00may  */
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
